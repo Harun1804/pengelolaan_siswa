892902356b66dc246acc5294bce28eebc5a8434f
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-06-07 06:16:13 +0700

    initial

---
+++ database/migrations/2021_06_06_231158_create_siswa_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_depan',30);
+            $table->string('nama_belakang',30);
+            $table->enum('jenis_kelamin',['laki - laki','perempuan']);
+            $table->enum('agama',['islam','kristen','katolik','hindu','budha']);
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+}
